519f0ae924c5ead2e890c30a0c559dc8d0049db5
Dima <belousd2007@gmail.com>
2025-12-18 16:46:27 +0100

    Add users - 13 Update Program.cs

--- WeatherStation/Program.cs
+++ WeatherStation/Program.cs
@@ -4,7 +4,18 @@ class Program
        static void Main(string[] args)
        {
             User user1 = new User(999, "Ja");
-            User user2 = new User(999, "Ja");
-            Sensor sensor1 = new Sensor();
+            User user2 = new User(998, "Jaj");
+			User user3 = new User(1, "Rom");
+			User user4 = new User(2, "Dil");
+			User user5 = new User(3, "Syr");
+			User user6 = new User(4, "Tuhliak");
+			User user7 = new User(5, "Sitr");
+			User user8 = new User(6, "Paj");
+			User user9 = new User(7, "Saj");
+			User user10 = new User(8, "Naj");
+			User user11 = new User(9, "Kafka");
+			User user12 = new User(10, "Tajka");
+			User user13 = new User(11, "Jakor");
+
        }
 }
