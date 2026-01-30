ee544abe25668175ca0678f7e0d5077d865cc60a
Pavan <thebardyt@gmai.com>
2026-01-30 16:02:13 +0100

    Dodany nowy projekt Reposytyoryj i w nim Clase UserReposytowy i interfejs IUserRepository.

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly WetherStationContext _dbContext;
+        
+        public UserRepository(WetherStationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public User? GetUser(int id)
+        {
+            return _dbContext.Set<User>().FirstOrDefault(x => x.Id == id);
+            
+        }
+    }
+}
