ad98ddf9c84a91b01c823246924fe170c627c6f2
Pavan <thebardyt@gmai.com>
2026-01-30 16:02:13 +0100

    k

--- Database/WetherStationContext.cs
+++ Database/WetherStationContext.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace Database
 {
@@ -13,7 +16,35 @@ namespace Database
         public DbSet<Sensor> Sensors { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WetherStationDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WetherStationDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+                .UseSeeding((context, _) =>
+                {
+                var users = context.Set<User>().FirstOrDefault();
+                if (users == null)
+                {
+                    var userFaker = new Faker<User>();
+                    userFaker.RuleFor(x => x.Name, f => f.Name.FullName());
+                    var usersToAdd = userFaker.Generate(1000);
+                    context.AddRange(usersToAdd);
+                    context.SaveChanges();
+                }
+            });
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WetherStationDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False")
+                .UseSeeding((context, _) =>
+                {
+                    var sensors = context.Set<Sensor>().FirstOrDefault();
+                    if (sensors == null)
+                    {
+                        var sensorFaker = new Faker<Sensor>();
+                        sensorFaker.RuleFor(x => x.Name, f => f.Name.FullName());
+                        sensorFaker.RuleFor(x => x.Type, f => f.Lorem.Word());
+                        sensorFaker.RuleFor(x => x.MeasurementDate, f => f.Date.Past());
+                        var sensorToAdd = sensorFaker.Generate(1000);
+                        context.AddRange(sensorToAdd);
+                        context.SaveChanges();
+                    }
+                });
         }
+
     }
 }
